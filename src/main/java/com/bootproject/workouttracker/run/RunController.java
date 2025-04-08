357829476511cb48046b7ae6247a43d45b7037a7
Sujung <rlatn1993@naver.com>
2025-04-08 19:08:17 -0400

    Post methods added : create, update, delete

--- src/main/java/com/bootproject/workouttracker/run/RunController.java
+++ src/main/java/com/bootproject/workouttracker/run/RunController.java
@@ -4,9 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -35,4 +40,24 @@ public class RunController {
         return run.get();
     }
 
+    // post
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    void create(@RequestBody Run run) {
+        runRepository.create(run);
+    }
+
+    // put
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @PutMapping("/{id}")
+    void update(@RequestBody Run run, @PathVariable Integer id) {
+        runRepository.update(run, id);
+    }
+
+    // delete
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable Integer id) {
+        runRepository.delete(id);
+    }
 }
--- src/main/java/com/bootproject/workouttracker/run/RunRepository.java
+++ src/main/java/com/bootproject/workouttracker/run/RunRepository.java
@@ -30,4 +30,18 @@ public class RunRepository {
         return runs.stream().filter(run -> run.id() == id).findFirst();
     }
 
+    void create(Run run) {
+        runs.add(run);
+    }
+
+    void update(Run run, Integer id) {
+        Optional<Run> existingRun = findById(id);
+        if (existingRun.isPresent()) {
+            runs.set(runs.indexOf(existingRun.get()), run);
+        }
+    }
+
+    void delete(Integer id) {
+        runs.removeIf(run -> run.id().equals(id));
+    }
 }
